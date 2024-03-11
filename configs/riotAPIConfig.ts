079927601baa95e3e80d389b2c10c403e834a06d
Vischerkarpfen <nikki@vischer.de>
2024-03-11 23:16:43 +0100

    Changed riotAPIConfig.ts

--- configs/riotAPIConfig.ts
+++ configs/riotAPIConfig.ts
@@ -1,9 +1,11 @@
 interface RiotAPIConfig {
     apiKey: string;
     region: string;
+    specific_region: string;
 }
 
 const riotAPIConfig: RiotAPIConfig = {
     apiKey: "",
     region: "",
+    specific_region: "",
 };
